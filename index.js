95070b61777f4797f41573c96da4189280a1b825
Hadi Abaza <Hadiabaza1@gmail.com>
2021-05-20 19:33:29 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, surprise) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+  }
+  return newArr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
